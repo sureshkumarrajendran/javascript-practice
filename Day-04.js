257369d68d85b1b68bea78b03f6a5733c390e5d5
sureshkumarrajendran <resh.kumar2016@gmail.com>
2022-08-05 12:25:37 +0530

    Day-02

---
+++ Day-04.js
@@ -0,0 +1,123 @@
+// conditional statements
+// if, if-else , else if ladder, switch
+
+// let age = 18;
+// if( age >=18) {
+//     console.log("Elogible for vote");
+// }
+// else{
+// console.log("Not Eligible for vote");
+// }
+
+// && -> logical and gate
+// let age = 18;
+// let isVoterIDAvailable = true;
+
+// if (age >= 18 && isVoterIDAvailable) {
+//   console.log("Elogible for vote");
+// } else {
+//   console.log("Not Eligible for vote");
+// }
+
+// || -> logical or gate
+// let isAadharAvailable =true;
+// let isVoterIDAvailable =true;
+// if(isAadharAvailable || isVoterIDAvailable){
+//     console.log("Eligible to get passport");
+// }else{
+//     console.log(" Not Eligible to get passport");
+// }
+
+// let num1 = 5;
+// let num2 = 10;
+// if (num1 > num2) {
+//   console.log("num1 is greater");
+// } else {
+//   console.log("num2 is a greater");
+// }
+
+//greatest of 3 nums
+// logical operators
+// && -> logical and
+// || -> logical or
+// !  -> logical not
+
+// let num1 = 5;
+// let num2 = 10;
+// let num3 = 20;
+
+// if (num1 > num2 && num1 > num3) {
+//   console.log("num1 is greater");
+// } else if (num2 > num1 && num2 > num3) {
+//   console.log("num2 is greater");
+// } else {
+//   console.log("num3 is greater");
+// }
+
+//practice
+// if (10) {
+//   console.log("suresh");
+// }
+// if (-10) {
+//   console.log("suresh");
+// }
+// if (0) {
+//   console.log("suresh");
+// }
+// if ("suresh") {
+//   console.log("suresh");
+// }
+// if ("") {
+//   console.log("suresh");
+// }
+// if (null) {
+//   console.log("suresh");
+// }
+// if (undefined) {
+//   console.log("suresh");
+// }
+
+// if (nan) {
+//   console.log("suresh");
+// }
+//###################################
+// falsy values
+// 0, false, null, undefined, "" , NaN.
+//###################################
+
+// console.log(1>2>3); // 1>2 = FALSE(0), then 0>3 is false
+// console.log(3>2>1); // 3 >2 =TRUE(1), then 1>1 is also false.
+
+// console.log(console.log("suresh")); // print name but undefined
+
+// short circuit
+// and && both stmt has to be true, 1st check the 1st stmt.
+// let age=20;
+// if(age >=18 && console.log("suresh");){
+//     console.log("eligible");
+// }else{
+//     console.log("not Eligible");
+// }
+
+// or || any one can be true if the 1t stmmt is FALSE then move to 2nd , if 1st is TRUE then is print the log.
+// let age=20;
+// if(age >=18 || console.log("suresh");){
+//     console.log("eligible");
+// }else{
+//     console.log("not Eligible");
+// }
+
+// logical Not !
+// true -> false
+// false -> true
+
+// if (!(10 > 5)) {
+//   console.log(10);
+// } else {
+//   console.log(5);
+// }
+
+// practice
+// the not ! operator will change the trueto false.
+// console.log(!10);// false
+// console.log(!null, !0, !""); //TRUE
